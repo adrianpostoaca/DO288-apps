191e69fce87b3c0239f800f180d3fc6281548b64
Adrian Postoaca <adrian.postoaca@yahoo.com>
2026-08-07 14:34:11 +0300

    Add health check endpoints /livez and /readyz

--- kubefiles/deploy-introduction/server.js
+++ kubefiles/deploy-introduction/server.js
@@ -3,9 +3,19 @@ const hostname = '0.0.0.0';
 const port = 4000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  if (req.url === '/livez') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('OK\n');
+  } else if (req.url === '/readyz') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('OK\n');
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  }
 });
 
 server.listen(port, hostname, () => {
